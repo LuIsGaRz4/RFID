3023bf7518e081ca5bbead741982ddcf345bba20
Luis Garza <luis.garza@landisgyr.com>
2025-07-15 11:00:44 -0500

    cambios url

--- src/app/services/rfid.service.ts
+++ src/app/services/rfid.service.ts
@@ -9,7 +9,7 @@ import { RFIDUsuario } from '../MODELS/rfid-registro.models';
   providedIn: 'root',
 })
 export class RfidService {
-  private baseUrl = 'https://api-rfid-dxc7ajbaf3encafu.canadacentral-01.azurewebsites.net/api/RFIDRegistro';
+  private baseUrl = 'https://https://api-rfid-dxc7ajbaf3encafu.canadacentral-01.azurewebsites.net/api/RFIDRegistro';
 
   constructor(private http: HttpClient) {}
 
